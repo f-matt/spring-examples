b0ab20a5a01333f6e054b58ea512086efce1dd53
fmatt <mattioli.fernando@gmail.com>
2014-09-09 17:10:59 -0300

    Spring config added

---
+++ spring-binding/src/main/java/org/example/model/Foo.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+public class Foo {
+
+	private int id;
+
+	private String description;
+
+	private Bar bar;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Bar getBar() {
+		return bar;
+	}
+
+	public void setBar(Bar bar) {
+		this.bar = bar;
+	}
+
+}
